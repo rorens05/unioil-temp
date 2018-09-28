a2a6ef15b17ca8891c18d9744bbbf25e5a17753d
Rogelio Mendeja <rmendeja@yondu.com>
2018-09-28 18:32:50 +0800

    My Profile -

--- src/containers/private/MyProfile/View/MyProfileView.js
+++ src/containers/private/MyProfile/View/MyProfileView.js
@@ -12,7 +12,7 @@ export default class MyProfileView extends Component {
 
   render() {
     
-    const { userAccountInfo } = this.props;
+    const { userInfo } = this.props;
     
     return (
       <div>
@@ -21,24 +21,24 @@ export default class MyProfileView extends Component {
         />
         <div style={{display: 'flex', alignItems: 'center', padding: '30px', background: '#F3F3F6' , border:'1px solid #E6ECF5'}} >
             <Avatar size={80} icon="user" style={{color: '#5a5e76' , backgroundColor: '#B8BBC9'}} />
-            <h1 style={{margin: '0 20px', color: '#5a5e76', fontSize: '40px' }}>Francine Narciso</h1>
+            <h1 style={{margin: '0 20px', color: '#5a5e76', fontSize: '40px' }}>{userInfo && `${userInfo.firstname} ${userInfo.lastname} `} Narciso</h1>
         </div>
         <div style={{padding: '30px',  border:'1px solid #E6ECF5', borderTop: 0}}>
             <div>
               <h2 style={{fontWeight: 'bold', fontSize: '20px'}}>My Information</h2>
               <Row>
-                <Col span={18} push={3}>{'fnarciso'}</Col>
+                <Col span={18} push={3}>{userInfo && userInfo.username}</Col>
                 <Col span={3} pull={18}><span style={{fontWeight: '600'}}>UserName:</span></Col>
               </Row>
               <Row>
-                <Col span={18} push={3}><a style={{color: '#1890FF'}} href={`mailto:${'loyalty.officer@unioil.com'}`}>loyalty.officer@unioil.com</a></Col>
+                <Col span={18} push={3}><a style={{color: '#1890FF'}} href={`mailto:${userInfo && userInfo.email}`}>loyalty.officer@unioil.com</a></Col>
                 <Col span={3} pull={18}><span style={{fontWeight: '600'}}>Email:</span></Col>
               </Row>
             </div>
             <div style={{margin: '12px 0 50px'}}>
               <h2 style={{fontWeight: 'bold', fontSize: '20px'}}>Access Role</h2>
               <Row>
-                <Col span={18} push={3}>{'Administrator'}</Col>
+                <Col span={18} push={3}>{userInfo && userInfo.role == "1" ? "System Admin": userInfo && "Marketing Personnel" }</Col>
                 <Col span={3} pull={18}><span style={{fontWeight: '600'}}>Role:</span></Col>
               </Row>
             </div>
--- src/containers/private/MyProfile/index.js
+++ src/containers/private/MyProfile/index.js
@@ -1,29 +1,52 @@
+// LIBRARIES
 import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { notification, Icon } from "antd"
 
+// COMPONENTS
 import MyProfileView from './View/MyProfileView';
 import MainContent from '../../../components/Dashboard/Layout/components/MainContent';
 import { PAGE404 } from "components/PageError/index"
 
+// HELPER FUNCTIONS
+import { API_UNI_OIL } from "utils/Api";
+
+
 class MyProfile extends Component {
   state = {
     pageRoutes: [  
-    ],    
+    ],
+    mounted: false,
+    userInfo: null
   }
+  
+  async componentDidMount() {
 
-  componentDidUpdate() {
-   
+    const { match } = this.props;
+
+    API_UNI_OIL.post(`adminProfile`)        
+    .then((response) => {
+      this.setState({
+        userInfo: {...response.data.data[0]},
+        mounted: true
+      })
+    })
+    .catch(({response: error}) => {
+      notification.error({ message: "Error", description: error.data.message , duration: 20, });
+      this.setState({ mounted: false })
+    });
+    
   }
 
   render() {       
     
-    const { pageRoutes } = this.state  
+    const { pageRoutes, userInfo } = this.state  
 
     return (
     <div style={{position: 'relative'}}>
       <MainContent pageRoutes={pageRoutes}>
         <Switch>
-          <Route exact path = "/my-profile" component = { MyProfileView } />
+          <Route exact path = "/my-profile" render = {(props)=> <MyProfileView userInfo={userInfo} /> }/>
           <PAGE404 />
         </Switch>
       </MainContent>
@@ -32,4 +55,4 @@ class MyProfile extends Component {
   }
 }
 
-export default MyProfile;
+export default withRouter(MyProfile);
