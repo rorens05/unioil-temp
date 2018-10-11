c601122c39158653e3f35564570f0ac06360d96e
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-11 11:14:03 +0800

    Lock Account - View

--- src/containers/private/MemberManagement/LockAccount/View/components/LockAccountViewForm.js
+++ src/containers/private/MemberManagement/LockAccount/View/components/LockAccountViewForm.js
@@ -50,15 +50,15 @@ function LockAccountViewForm(props) {
                 <Col span={3} pull={18}><span style={{fontWeight: '600'}}>Account Status:</span></Col>
               </Row>
               <Row>
-                <Col span={18} push={3}>{'Need Clarification'}</Col>
+                <Col span={18} push={3}>{userInfo && userInfo.reason}</Col>
                 <Col span={3} pull={18}><span style={{fontWeight: '600'}}>Reason:</span></Col>
               </Row>
               <Row>
-                <Col span={18} push={3}>{'Need Clarification'}</Col>
+                <Col span={18} push={3}>{userInfo && userInfo.lock_dt}</Col>
                 <Col span={3} pull={18}><span style={{fontWeight: '600'}}>Locked Date:</span></Col>
               </Row>
               <Row>
-                <Col span={18} push={3}>{'Need Clarification'}</Col>
+                <Col span={18} push={3}>{userInfo && userInfo.unlocked_by}</Col>
                 <Col span={3} pull={18}><span style={{fontWeight: '600'}}>Unlocked By:</span></Col>
               </Row>
             </div>
