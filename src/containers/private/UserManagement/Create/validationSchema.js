95c89bd2b8702f73b580280b9e1366e72299863e
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-23 18:35:09 +0800

    DEF - 017 - Bug Fix

--- src/containers/private/UserManagement/Create/validationSchema.js
+++ src/containers/private/UserManagement/Create/validationSchema.js
@@ -6,25 +6,25 @@ export const userDetailsSchema = Yup.object().shape({
     .trim()
     .required('Username is required!')
     .matches(
-      /^[a-zA-Z0-9_@. ]+$/,
+      /^[a-zA-Z0-9_@.ñÑ ]+$/,
       {
-        message: 'Invalid username.',
+        message: 'Invalid Username.',
         excludeEmptyString: true,
       },
     ),
   firstname: Yup.string()
     .trim()
-    .matches(/^[A-Za-z -]+$/, { excludeEmptyString: false, message: "Invalid characters" })
+    .matches(/^[A-Za-z ñÑ-]+$/, { excludeEmptyString: false, message: "Invalid First Name" })
     .required('First Name is required!'),
   lastname: Yup.string()
     .trim()
-    .matches(/^[A-Za-z -]+$/, { excludeEmptyString: false, message: "Invalid characters" })
+    .matches(/^[A-Za-z ñÑ-]+$/, { excludeEmptyString: false, message: "Last Name" })
     .trim()
     .required('Last Name is required!'),
   email: Yup.string()
     .trim()
     .required('Email is required!')
-    .matches(/^[A-Za-z0-9@_. ]+$/, { excludeEmptyString: false, message: "Invalid characters" })
+    .matches(/^[A-Za-z0-9@_.ñÑ ]+$/, { excludeEmptyString: false, message: "Email Address" })
     .email("Invalid Email"),
   role: Yup.string()
     .required('Role is required!'),
