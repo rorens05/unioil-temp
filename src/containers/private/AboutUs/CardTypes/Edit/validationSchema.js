a322b37267c35d1acc72e4edb3cd81c7976c00ac
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-24 16:56:09 +0800

    Card Types - max character

--- src/containers/private/AboutUs/CardTypes/Edit/validationSchema.js
+++ src/containers/private/AboutUs/CardTypes/Edit/validationSchema.js
@@ -3,17 +3,27 @@ import * as Yup from 'yup'
 
 export const userDetailsSchema = Yup.object().shape({
   code: Yup.string()
-    .required('Card Code is required!')
-    .max(12,"Card Code must be 12 characters only. "),
+    .trim()
+    .max(128, "Maximum character is 128.")
+    //.max(12,"Card Code must be 12 characters only. "),
+    .required('Card Code is required!'),
   type: Yup.string()
-    .required('Card Type is required!'),
+    .trim()
+    .max(128, "Maximum character is 128.")
+    .required('Card Type Description is required!'),
   description: Yup.string()
-    .required('Description is required!'),
+    .trim()
+    .max(32000, "Maximum character is 32,000.")
+    .required('Card Type Short Description is required!'),
   // image: Yup.image()
   // .required('Image is required!'),
   terms_and_conditions: Yup.string()
+    .trim()
+		.max(32000, "Maximum character is 32,000.")
     .required('Terms and Condition is required!'),
   faqs: Yup.string()
+    .trim()
+		.max(32000, "Maximum character is 32,000.")
     .required('FAQs is required!')
 })
 
