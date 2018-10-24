e48a937ef1d88e376983ec4ebcdaf8349299fe49
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-24 16:27:14 +0800

    User Management - max character

--- src/containers/private/UserManagement/Edit/validationSchema.js
+++ src/containers/private/UserManagement/Edit/validationSchema.js
@@ -4,6 +4,7 @@ import * as Yup from 'yup'
 export const userDetailsSchema = Yup.object().shape({
   username: Yup.string()
     .trim()
+    .max(128, "Maximum character is 128.")
     .required('Username is required!')
     .matches(
       /^[a-zA-Z0-9_@.ñÑ ]+$/,
@@ -14,13 +15,16 @@ export const userDetailsSchema = Yup.object().shape({
     ),
   firstname: Yup.string()
     .trim()
+    .max(128, "Maximum character is 128.")
     .matches(/^[A-Za-z ñÑ-]+$/, { excludeEmptyString: false, message: "Invalid First Name" })
     .required('First Name is required!'),
   lastname: Yup.string()
     .trim()
+    .max(128, "Maximum character is 128.")
     .matches(/^[A-Za-z ñÑ-]+$/, { excludeEmptyString: false, message: "Invalid Last Name" })
     .required('Last Name is required!'),
   email: Yup.string()
+    .max(128, "Maximum character is 128.")
     .required('Email is required!')
     .matches(/^[A-Za-z0-9@_.ñÑ ]+$/, { excludeEmptyString: false, message: "Invalid Email Address" })
     .email("Invalid Email Address"),
