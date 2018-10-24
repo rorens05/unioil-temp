7855620af325768e8f48fc8ca611690f3b5e1f30
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-24 16:45:50 +0800

    Top Up - max character

--- src/containers/private/TopUp/Edit/validationSchema.js
+++ src/containers/private/TopUp/Edit/validationSchema.js
@@ -5,8 +5,9 @@ export const userDetailsSchema = Yup.object().shape({
   fee_code: Yup.string()
     .required('Free Code is required!'),
   name: Yup.string()
-    .required('Name is required!')
     .trim()
+    .max(128, "Maximum character is 128.")
+    .required('Name is required!')
     .matches(/^[A-Za-z0-9.ñÑ ]+$/, { excludeEmptyString: false, message: "Invalid Name" }),
   amount: Yup.number()
     .required('Amount is required!'),
