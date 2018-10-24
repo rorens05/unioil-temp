674a8a559c01060a62ff698fd2a8d24ec0a2f85b
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-24 17:01:19 +0800

    System Parameters - max character

--- src/containers/private/SystemPreferences/Create/validationSchema.js
+++ src/containers/private/SystemPreferences/Create/validationSchema.js
@@ -5,13 +5,19 @@ export const userDetailsSchema = Yup.object().shape({
   logo: Yup.string(),
     //.required('logo is required!'),
   gps: Yup.string()
+    .trim()
+		.max(128, "Maximum character is 128.")
     .required('GPS Radius is required!'),
   contact_email_address_mobile: Yup.string()
+    .trim()
+		.max(128, "Maximum character is 128.")
     .email("Invalid Contact Email Address.")
     .required('Contact Email Address is required!'),
   contact_number_mobile: Yup.string()
     .required('Contact Number is required!'),
   contact_details: Yup.string()
+    .trim()
+		.max(128, "Maximum character is 128.")
     .email("Invalid Contact Details.")
     .required('Contact Details is required!')
 })
