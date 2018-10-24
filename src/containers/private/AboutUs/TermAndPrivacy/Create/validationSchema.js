22dcb0ffdf0c641a02c54bd9528aad2260ab496f
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-24 16:58:50 +0800

    Terms & Condition - max charcter

--- src/containers/private/AboutUs/TermAndPrivacy/Create/validationSchema.js
+++ src/containers/private/AboutUs/TermAndPrivacy/Create/validationSchema.js
@@ -3,8 +3,12 @@ import * as Yup from 'yup'
 
 export const userDetailsSchema = Yup.object().shape({
   title: Yup.string()
+    .trim()
+		.max(128, "Maximum character is 128.")
     .required('Title is required!'),
   details: Yup.string()
+    .trim()
+		.max(32000, "Maximum character is 32,000.")
     .required('Details is required!'),
   // type: Yup.string()
   //   .required('Type is required!')
