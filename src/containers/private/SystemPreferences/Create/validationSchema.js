b2c2146225eef2bdbf68b6d1689206057f510f70
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-24 14:34:44 +0800

    System Parameters- validation

--- src/containers/private/SystemPreferences/Create/validationSchema.js
+++ src/containers/private/SystemPreferences/Create/validationSchema.js
@@ -7,12 +7,12 @@ export const userDetailsSchema = Yup.object().shape({
   gps: Yup.string()
     .required('GPS Radius is required!'),
   contact_email_address_mobile: Yup.string()
-    .email("Invalid Email")
+    .email("Invalid Contact Email Address.")
     .required('Contact Email Address is required!'),
   contact_number_mobile: Yup.string()
     .required('Contact Number is required!'),
   contact_details: Yup.string()
-    .email("Invalid Email")
+    .email("Invalid Contact Details.")
     .required('Contact Details is required!')
 })
 
