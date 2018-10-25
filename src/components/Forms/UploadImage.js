79b44b741385e0fefc6fd45c530a735d3b048382
Rogelio Mendeja <rmendeja@yondu.com>
2018-10-25 18:18:37 +0800

    File Image - Message

--- src/components/Forms/UploadImage.js
+++ src/components/Forms/UploadImage.js
@@ -55,19 +55,23 @@ class UploadImage extends Component {
       message.error('You can only upload JPG or PNG file!');
     }
 
-    let fileSize;
-    let isLt2M;
+    let fileSize; let isLt2M;
+
     if(this.props.limit100kb) {
       fileSize = filesize(file.size, {output: "array"} ) // 100kb
       isLt2M = fileSize[0] < 104
     } else {
-      isLt2M = file.size / 1024 / 1024 < 2;
+      isLt2M = file.size / 1024 / 1024 < 2; // 2MB
     }
     //const isLt2M = fileSize[0] < 104;
     //const isLt2M = file.size / 1024 / 1024 < 2;
    
     if (!isLt2M) {
-      message.error('Image must smaller than 100KB!');
+      if(this.props.limit100kb) {
+        message.error('Image must smaller than 100KB!');
+      } else {
+        message.error('Image must smaller than 2MB!');
+      }
     }
     return isJPG && isLt2M;
     
