ed9f6da024eb5c5c716bf489f01a66bc9845284e
Rogelio Mendeja <rmendeja@yondu.com>
2018-11-19 10:37:36 +0800

    Photo Slider -  Edit - remove static image path

--- src/containers/private/HomePage/PhotoSlider/Edit/components/EditPhotoSliderForm.js
+++ src/containers/private/HomePage/PhotoSlider/Edit/components/EditPhotoSliderForm.js
@@ -91,7 +91,7 @@ function EditUserManagementForm(props) {
         type="file"
         accept=".jpg , .png, .gif"
         multiple={false}
-        imageUrl={props.values.image && `${process.env.REACT_APP_IMG_URL}${props.values.image}`}
+        imageUrl={props.values.image && `${props.values.image}`}
         className="upload-list-inline"
         icon="user"
         layout={formItemLayout}
