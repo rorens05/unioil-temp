36818fce54abe29ba54f71af376d6a79af87c6bb
Rogelio Mendeja <rmendeja@yondu.com>
2018-12-20 16:13:42 +0800

    Image Height

--- src/components/Forms/UploadImage.js
+++ src/components/Forms/UploadImage.js
@@ -153,7 +153,7 @@ class UploadImage extends Component {
                   onChange={this.normFile}
                   className="upload-image"
                 >
-                  {imageUrl ? <img src={imageUrl} alt="avatar" width={imgStyle ? imgStyle.width : "100%" } height={imgStyle ? imgStyle.height : "100"} /> : uploadButton}
+                  {imageUrl ? <img src={imageUrl} alt="avatar" width={imgStyle ? imgStyle.width : "100%" } height={imgStyle ? imgStyle.height : "135"} /> : uploadButton}
                   <div style={{width: imgWidth ? imgWidth : 'initial', margin: '0 auto'}}>
                     <p className="ant-upload-text">Click or drag file to this area to upload.</p>
                     <p className="ant-upload-hint">Support for a single upload only.</p>
