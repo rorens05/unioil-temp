688dbdd3325fef291a30528f3e38a9451ace0762
Rogelio Mendeja <rmendeja@yondu.com>
2019-04-05 11:13:11 +0800

    Card Type - View Page for new fields cover image

--- src/containers/private/AboutUs/CardTypes/View/components/ViewCardForm.js
+++ src/containers/private/AboutUs/CardTypes/View/components/ViewCardForm.js
@@ -21,19 +21,19 @@ function ViewCardForm(props) {
               <h2 style={{margin: '0 0 20px'}}>Details</h2>
               {/*Account Details */}
               <h2 style={{fontWeight: 'bold', fontSize: '16px'}}>CARD DETAILS</h2>
-              <Row>
+              <Row style={styles.marginTop}>
                 <Col span={18} push={5}>{userInfo && userInfo.code}</Col>
                 <Col span={5} pull={18}><span style={{fontWeight: '600'}}>Card Code:</span></Col>
               </Row>
-              <Row>
+              <Row style={styles.marginTop}>
                 <Col span={18} push={5}>{userInfo && userInfo.name}</Col>
                 <Col span={5} pull={18}><span style={{fontWeight: '600'}}>Card Type:</span></Col>
               </Row>
-              <Row>
+              <Row style={styles.marginTop}>
                 <Col span={18} push={5}>{userInfo && userInfo.description}</Col>
                 <Col span={5} pull={18}><span style={{fontWeight: '600'}}>Short Description:</span></Col>
               </Row>
-              <Row>
+              <Row style={styles.marginTop}>
                 <Col span={18} push={5}>
                     <img 
                       src={userInfo && userInfo.image && `${userInfo.image}`} 
@@ -42,25 +42,32 @@ function ViewCardForm(props) {
                       style={{maxHeight: '250px'}}
                     />
                 </Col>
-                <Col span={5} pull={18}><span style={{fontWeight: '600'}}>Image:</span></Col>
+                <Col span={5} pull={18}><span style={{fontWeight: '600'}}>Card Type Image:</span></Col>
               </Row>
-              <Row>
+              <Row style={styles.marginTop}>
+                <Col span={18} push={5}>
+                    <img 
+                      src={userInfo && userInfo.image && `${userInfo.bg_image}`} 
+                      alt="avatar"
+                      width="300"
+                      style={{maxHeight: '250px'}}
+                    />
+                </Col>
+                <Col span={5} pull={18}><span style={{fontWeight: '600'}}>Cover Card Type Image:</span></Col>
+              </Row>
+              <Row style={styles.marginTop}>
                 <Col span={18} push={5}>{userInfo && userInfo.id_number && userInfo.id_number ? "Yes": "No"}</Col>
                 <Col span={5} pull={18}><span style={{fontWeight: '600'}}>ID Number Required:</span></Col>
               </Row>
-              <Row>
-                <Col span={18} push={5}>{userInfo && userInfo.id_number_description}</Col>
-                <Col span={5} pull={18}><span style={{fontWeight: '600'}}>Required ID Type Description:</span></Col>
-              </Row>
             </div>
              {/*Account Details */}
-            <div style={{margin: '12px 0'}}>
+            <div style={{margin: '15px 0 10px'}}>
               <h2 style={{fontWeight: 'bold', fontSize: '16px'}}>DATA PRIVACY DETAILS</h2>
-              <Row>
+              <Row style={styles.marginTop}>
                 <Col span={18} push={5}>{userInfo && userInfo.terms_and_conditions}</Col>
                 <Col span={5} pull={18}><span style={{fontWeight: '600'}}>{`Terms & Conditions:`}</span></Col>
               </Row>
-              <Row>
+              <Row style={styles.marginTop}>
                 <Col span={18} push={5}>{userInfo && userInfo.faqs}</Col>
                 <Col span={5} pull={18}><span style={{fontWeight: '600'}}>FAQs:</span></Col>
               </Row>
@@ -80,3 +87,9 @@ ViewCardForm = connect(
 
 export default ViewCardForm;
 
+
+const styles = {
+  marginTop: {
+    marginTop: '5px'
+  }
+}
