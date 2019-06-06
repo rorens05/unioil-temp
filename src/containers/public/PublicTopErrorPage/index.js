d8245d6a8af698b637cecb049b770c8d307ec3fd
Rogelio Mendeja <rmendeja@yondu.com>
2019-06-06 12:55:27 +0800

    Top Up - adjust layout for mobile iOS

--- src/containers/public/PublicTopErrorPage/index.js
+++ src/containers/public/PublicTopErrorPage/index.js
@@ -25,28 +25,23 @@ class PublicTopErrorPage extends Component {
   render() {
 
     return (
-      <div align="center" id="1" style={{position: 'absolute', top: '50%', 
-          left: 0, right: 0, transform: 'translateY(-50%)', height: '100%'}}>
-           
-
-          <div style={{marginTop: '16vmin'}}>
-          <div>
+      <div id="1">
+          <div style={{...styles.topAlign}}>
             <img src={ require("assets/img/ic_error.svg") } style={{ width: '24vmin'}} />
             <p style={{ lineHeight: '7vmin', fontSize: '6vmin', fontWeight: 'bold', marginTop: '5vmin', color:'#4D4D4D' }}>
               Transaction Failed.<br/>
               Your purchase for top-up is not <br/>successful. Please try again.
             </p>
-            <div style={{position: 'fixed', bottom: 0,width: '100%' , marginBottom: '3vmin'}}>
-              <Button 
-                onClick={this.backHandler} 
-                type="primary" id="backError" size="large" 
-                style={{backgroundColor: '#e74610', 
-                  borderColor: '#e74610', margin: '15px', width: '90%', height: '44px'
-                }}>
-                Return to Top-Up Page
-              </Button>
-            </div>
           </div>
+          <div style={{...styles.bottomAlign}}>
+            <Button 
+              onClick={this.backHandler} 
+              type="primary" id="backError" size="large" 
+              style={{backgroundColor: '#e74610', 
+                borderColor: '#e74610', width: '100%', height: '11vmin', fontSize: '5vmin',lineHeight: 1
+              }}>
+              Return to Top-Up Page
+            </Button>
           </div>
       </div>
     )
@@ -55,3 +50,23 @@ class PublicTopErrorPage extends Component {
 
 
 export default PublicTopErrorPage;
+
+
+const styles = {
+  bottomAlign: {
+    position: 'absolute',
+    right: '0',
+    bottom: '0',
+    left: '0',
+    padding: '1rem',
+    textAlign: 'center'
+  },
+  topAlign: {
+    position: 'absolute',
+    right: '0',
+    top: '0',
+    left: '0',
+    padding: '1rem',
+    textAlign: 'center'
+  },
+}
--- src/containers/public/PublicTopSuccessPage/index.js
+++ src/containers/public/PublicTopSuccessPage/index.js
@@ -74,11 +74,10 @@ class PublicTopSuccessPage extends Component {
     const { loading, status, userInfo, message } = this.state;
 
     return (
-      <div align="center" id="1" style={{position: 'absolute', top: '50%', 
-          left: 0, right: 0, transform: loading ? 'initial' :'translateY(-50%)' , height: '100%'}}>
+      <div id="1">
           {
-            loading 
-              ? <div style={{fontSize: '6vmin'}}>
+            !loading 
+              ? <div style={{fontSize: '6vmin',...styles.topAlign, top:'45%'}}>
                   <Icon type="sync" spin /> Loading Top-Up Please wait...
               </div>
               :
@@ -87,43 +86,45 @@ class PublicTopSuccessPage extends Component {
                   status == 200 
                     ? 
                     <div>
-                      <img src={ require("assets/img/ic_success.svg") } style={{ width: '18vmin'}} />
-                      <p style={{ lineHeight: '7vmin', fontSize: '6vmin', 
-                          fontWeight: 'bold', marginTop: '5vmin', marginBottom: 0
-                      }}>
+                      <div style={{...styles.topAlign}}>
+                        <img src={ require("assets/img/ic_success.svg") } style={{ width: '18vmin'}} />
+                        <p style={{ lineHeight: '7vmin', fontSize: '6vmin', 
+                            fontWeight: 'bold', marginTop: '5vmin', marginBottom: 0
+                        }}>
                         Your purchase for top-up points <br/> is successful!
-                      </p>
-                      <div style={{fontSize: '15px', fontWeight: 400}}>
-                        <Row>
-                          <Col span={24}>
-                            <span style={{fontSize: '6vmin', fontWeight: 'bold', marginTop: '5vmin'}}>
-                              {userInfo && userInfo.date}
-                            </span>
-                          </Col>
-                          <Col span={24}>{userInfo && `Card Number ${userInfo.card_number}`}</Col>
-                          <Col span={24}>{userInfo && `Sales Invoice Number ${userInfo.invoice}`}</Col>
-                        </Row>
-                        <div style={{marginTop: '5vmin' , fontSize: '3.5vmin'}}>
-                          <Row type="flex" justify="space-around">
-                            <Col span={8} style={{textAlign: 'left'}}>Top-up Points Value</Col>
-                            <Col span={8} style={{textAlign: 'right'}}>{userInfo && ` ${userInfo.points}`}</Col>
+                        </p>
+                        <div style={{fontSize: '15px', fontWeight: 400}}>
+                          <Row>
+                            <Col span={24}>
+                              <span style={{fontSize: '6vmin', fontWeight: 'bold', marginTop: '5vmin'}}>
+                                {userInfo && userInfo.date}
+                              </span>
+                            </Col>
+                            <Col span={24}>{userInfo && `Card Number ${userInfo.card_number}`}</Col>
+                            <Col span={24}>{userInfo && `Sales Invoice Number ${userInfo.invoice}`}</Col>
                           </Row>
-                          <Row type="flex" justify="space-around">
-                            <Col span={8} style={{textAlign: 'left'}}>Payment Value</Col>
-                            <Col span={8} style={{textAlign: 'right'}}>{userInfo && `Php ${userInfo.payment_val}`}</Col>
-                          </Row>
-                          <Row type="flex" justify="space-around">
-                            <Col span={8} style={{textAlign: 'left'}}>Paypal Fee</Col>
-                            <Col span={8} style={{textAlign: 'right'}}>{userInfo && `Php ${userInfo.paypal_fee}`}</Col>
-                          </Row>
+                          <div style={{marginTop: '5vmin' , fontSize: '3.5vmin'}}>
+                            <Row type="flex" justify="space-around">
+                              <Col span={8} style={{textAlign: 'left'}}>Top-up Points Value</Col>
+                              <Col span={8} style={{textAlign: 'right'}}>{userInfo && ` ${userInfo.points}`}</Col>
+                            </Row>
+                            <Row type="flex" justify="space-around">
+                              <Col span={8} style={{textAlign: 'left'}}>Payment Value</Col>
+                              <Col span={8} style={{textAlign: 'right'}}>{userInfo && `Php ${userInfo.payment_val}`}</Col>
+                            </Row>
+                            <Row type="flex" justify="space-around">
+                              <Col span={8} style={{textAlign: 'left'}}>Paypal Fee</Col>
+                              <Col span={8} style={{textAlign: 'right'}}>{userInfo && `Php ${userInfo.paypal_fee}`}</Col>
+                            </Row>
+                          </div>
                         </div>
                       </div>
-                      <div style={{position: 'fixed', bottom: 0,width: '100%', marginBottom: '3vmin'}}>
+                      <div style={{...styles.bottomAlign}}>
                         <Button 
                           onClick={this.backHandlerSuccess}
                           type="primary" id="backSuccess" size="large" 
                           style={{backgroundColor: '#e74610', 
-                            borderColor: '#e74610', margin: '15px', width: '90%', height: '44px'
+                            borderColor: '#e74610', width: '100%', height: '11vmin', fontSize: '5vmin',lineHeight: 1
                           }}>
                           Return to Home Page
                         </Button>
@@ -131,18 +132,20 @@ class PublicTopSuccessPage extends Component {
                     </div>
                     : 
                     <div>
-                      <img src={ require("assets/img/ic_error.svg") } style={{ width: '24vmin'}} />
-                      <p style={{ lineHeight: '7vmin', fontSize: '6vmin', fontWeight: 'bold', marginTop: '5vmin', color:'#4D4D4D' }}>
-                        {/* Your purchase for top-up is not <br/>successful. Please try again. */}
-                        Transaction Failed.<br/>
-                        {message && `${message}.`}
-                      </p>
-                      <div style={{position: 'fixed', bottom: 0,width: '100%'}}>
+                      <div style={{...styles.topAlign}}>
+                        <img src={ require("assets/img/ic_error.svg") } style={{ width: '24vmin'}} />
+                        <p style={{ lineHeight: '7vmin', fontSize: '5vmin', fontWeight: 'bold', marginTop: '5vmin', color:'#4D4D4D' }}>
+                          {/* Your purchase for top-up is not <br/>successful. Please try again. */}
+                          Transaction Failed.<br/>
+                          {message && `${message}.`}
+                        </p>
+                      </div>
+                      <div style={{...styles.bottomAlign}}>
                         <Button 
                           onClick={this.backHandler} 
                           type="primary" id="backSuccess" size="large" 
                           style={{backgroundColor: '#e74610', 
-                            borderColor: '#e74610', margin: '15px', width: '90%', height: '44px'
+                            borderColor: '#e74610', width: '100%', height: '11vmin', fontSize: '5vmin',lineHeight: 1
                           }}>
                           Return to Top-Up Page
                         </Button>
@@ -158,3 +161,23 @@ class PublicTopSuccessPage extends Component {
 
 
 export default PublicTopSuccessPage;
+
+
+const styles = {
+  bottomAlign: {
+    position: 'absolute',
+    right: '0',
+    bottom: '0',
+    left: '0',
+    padding: '1rem',
+    textAlign: 'center'
+  },
+  topAlign: {
+    position: 'absolute',
+    right: '0',
+    top: '0',
+    left: '0',
+    padding: '1rem',
+    textAlign: 'center'
+  },
+}
